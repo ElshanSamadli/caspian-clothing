fa4eaa8962b14c038e5180d736d4ab730155e30b
Elshan Samadli <elshan.samadli@gmail.com>
2022-02-28 18:58:32 -0700

    Creating Components and Pages Section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-import HomePage from './HomePage'
+import HomePage from './Components/Pages/HomePage'
 
 function App() {
   return (
